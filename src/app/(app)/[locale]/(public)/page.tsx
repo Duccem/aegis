9a84bd92f39adc1321919db11e776eceaffd2cb7
Jose Manuel Veliz <jose.manuel.veliz.p@gmail.com>
2025-08-05 22:25:11 -0400

    Add initial project setup with Next.js, update dependencies, and create home page layout

---
+++ src/app/(app)/[locale]/(public)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="text-center text-lg">
+        Welcome to the Next.js application! This is the home page where you can
+        start building your app. Explore the features and enjoy coding with
+        Next.js!
+      </p>
+    </div>
+  );
+}
+
