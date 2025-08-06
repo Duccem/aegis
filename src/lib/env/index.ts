2095add12fc8d13327f5c1da170efee2061249d7
Jose Manuel Veliz <jose.manuel.veliz.p@gmail.com>
2025-08-05 22:27:58 -0400

    Add Zod for environment variable validation and create env configuration

---
+++ src/lib/env/index.ts
@@ -0,0 +1,34 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import z from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string(),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string().optional(),
+    GOOGLE_CLIENT_ID: z.string().optional(),
+    GOOGLE_CLIENT_SECRET: z.string().optional(),
+    GOOGLE_GENERATIVE_AI_API_KEY: z.string().optional(),
+    POLAR_ACCESS_TOKEN: z.string(),
+    SUCCESS_URL: z.string(),
+    POLAR_WEBHOOK_SECRET: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_SUPABASE_URL: z.string(),
+    NEXT_PUBLIC_SUPABASE_KEY: z.string(),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+    POLAR_ACCESS_TOKEN: process.env.POLAR_ACCESS_TOKEN,
+    SUCCESS_URL: process.env.SUCCESS_URL,
+    POLAR_WEBHOOK_SECRET: process.env.POLAR_WEBHOOK_SECRET,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+  },
+});
+
