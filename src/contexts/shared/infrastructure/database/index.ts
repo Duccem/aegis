e4caf29fc41601d97f5578d32de40ff0d11c96e6
Jose Veliz <ducen29@gmail.com>
2025-08-22 12:32:02 -0400

    feat: add postgres dependency and update database connection implementation

--- src/contexts/shared/infrastructure/database/index.ts
+++ src/contexts/shared/infrastructure/database/index.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import { env } from "../env";
 import * as schema from "./schema";
 
-export const database = drizzle(env.DATABASE_URL, { schema });
+const client = postgres(env.DATABASE_URL);
+
+export const database = drizzle(client, { schema });
